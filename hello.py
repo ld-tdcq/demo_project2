573d22cafd942638b3c4ce29a5b13fe8f6786533
ld_tdcq <liangding_tdcq@163.com>
2024-08-20 16:31:07 +0800

    修改了readme

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
 '''
 Author: liangd
 Date: 2024-08-20 16:13:26
+LastEditTime: 2024-08-20 16:30:18
+Description: 
+FilePath: \demo_project2\hello.py
+'''
+'''
+Author: liangd
+Date: 2024-08-20 16:13:26
 LastEditTime: 2024-08-20 16:15:13
 Description: 
 FilePath: \demo_project2\hello.py
