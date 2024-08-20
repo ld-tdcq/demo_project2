01a1dfd3e47eb314a66052e6ba78009848977139
ld_tdcq <liangding_tdcq@163.com>
2024-08-20 16:43:02 +0800

    11

---
+++ test.py
@@ -0,0 +1,15 @@
+'''
+Author: liangd
+Date: 2024-08-20 16:38:08
+LastEditTime: 2024-08-20 16:42:57
+Description: 
+FilePath: \demo_project2\test.py
+'''
+import numpy as np
+def add(x, y):
+    
+    return x + y
+
+if __name__ == '__main__' :
+    print(add(2, 3))
+    
